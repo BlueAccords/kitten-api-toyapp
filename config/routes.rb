2a6f27f201081c7f603ccf19a4497f4cb6ed572c
BlueAccords <myoldemailname@gmail.com>
2015-07-29 13:07:32 -0700

    Added kitten model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :kittens 
+  root 'KittensController#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
